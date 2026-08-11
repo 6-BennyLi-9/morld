931b1d5f346535848c1918ba9fd1099e39bfa806
BennyLi <bennyli2011@163.com>
2026-08-11 19:51:02 +0800

    revert

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,3 @@
-﻿mod util;
-
-#[cfg(test)]
+﻿#[cfg(test)]
 mod test{
-	use crate::util::types::VecEx;
-	use bevy::reflect::list::List;
-
-	#[test]
-	fn types_override() {
-		let mut vec = VecEx::new();
-		vec.0.push(1);
-		vec.0.push(2);
-		vec.0.push(3);
-
-		println!("{}", vec)
-	}
 }
