6ed47b71669f66361d645637ae3eb4e5b9209141
BennyLi <bennyli2011@163.com>
2026-08-11 17:13:40 +0800

    content

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 ﻿mod plugin_util;
+mod util;
 
 #[cfg(test)]
 mod test{
+
+	#[test]
+	fn types_override() {
+		let mut vec = super::util::content::ContentList::default();
+		vec.load_content(1);
+		vec.load_content(2);
+		vec.load_content(3);
+
+		println!("{}", vec)
+	}
 }
