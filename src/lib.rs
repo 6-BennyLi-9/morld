b1affaeebd0623a46745dcaca8e4f340de7561d6
BennyLi <bennyli2011@163.com>
2026-08-11 15:27:41 +0800

    plugin_util.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
-﻿#[cfg(test)]
+﻿mod plugin_util;
+
+#[cfg(test)]
 mod test{
 }
---
+++ src/plugin_util.rs
@@ -0,0 +1,29 @@
+﻿use bevy::app::{App, Plugin};
+use bevy::prelude::Component;
+use std::marker::PhantomData;
+
+///
+pub struct MorldPlugin<T> {
+	pub phantom_t: PhantomData<T>,
+}
+
+/// Implementation of `MorldPlugin` as a Plugin with
+/// a generic type parameter of the `Component` trait.
+impl<T: Component> Plugin for MorldPlugin<T> {
+	fn build(&self, app: &mut App) {
+		// app.add_systems(Startup, example_system::<T>);
+	}
+}
+
+impl<T> MorldPlugin<T> {
+	pub fn new() -> Self {
+		Self::default()
+	}
+}
+impl<T> Default for MorldPlugin<T> {
+	fn default() -> Self {
+		Self {
+			phantom_t: PhantomData,
+		}
+	}
+}
