741c094902a79bf088b78b7efc97ad1a8078f01b
BennyLi <bennyli2011@163.com>
2026-08-11 20:08:28 +0800

    新 types

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,1 +1,11 @@
-﻿pub mod morld_core;
+﻿use bevy::app::{PluginGroup, PluginGroupBuilder};
+
+pub mod types;
+
+pub struct MorldCore;
+
+impl PluginGroup for MorldCore {
+	fn build(self) -> PluginGroupBuilder {
+		PluginGroupBuilder::start::<Self>()
+	}
+}
