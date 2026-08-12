d3d18876c3eae224643fccf2dbdf6e0c2358026e
BennyLi <bennyli2011@163.com>
2026-08-12 16:55:45 +0800

    转为组件

--- src/core/types.rs
+++ src/core/types.rs
@@ -24,42 +24,24 @@ impl MorldCoreRuntime{
 }
 
 ///定义标准实体
-#[derive(Component, Debug)]
+#[derive(Component, Debug, Default)]
 pub struct Entity{
 	id: u32,
 	pos: Vec2,
 }
 
 ///标识携带生命值的实体
-trait WithHealth{
-	fn health_current(&self) -> u32;
-	fn health_maximum(&self) -> u32;
-	#[inline]
-	fn health_percentage(&self) -> f64 {
-		self.health_current() as f64 / self.health_maximum() as f64
-	}
-
-	/// returns: u32
-	/// - 溢出的治疗量
-	fn heal(&self, amount: u32) -> u32;
-
-	/// 如果目标不适配扩大生命值词条，返回 Err
-	fn health_expand(&self, amount: u32) -> Result<>;
+#[derive(Component, Debug, Default)]
+pub struct Carnal{
+	health: f32,
+	health_maximum: f32,
 }
 
 ///标识携带法力值的实体
-trait WithMana{
-	fn mana_current(&self) -> u32;
-	fn mana_maximum(&self) -> u32;
-	#[inline]
-	fn mana_percentage(&self) -> f64 {
-		self.mana_current() as f64 / self.mana_maximum() as f64
-	}
-
-	fn restore_mana(&self, amount: u32);
-
-	/// 如果目标不适配扩大法力值词条，返回 Err
-	fn mana_expand(&self, amount: u32) -> Result<>;
+#[derive(Component, Debug, Default)]
+pub struct Mage{
+	mana: f32,
+	mana_maximum: f32,
 }
 
 fn runtime_update(
