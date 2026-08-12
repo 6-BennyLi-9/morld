6c006206edb2e5b92cce16cba527f73ed2794e17
BennyLi <bennyli2011@163.com>
2026-08-12 22:47:53 +0800

    添加到 MorldCore

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,7 @@
 ﻿use crate::core::locales::FluentInitial;
 use crate::core::types::ResInitial;
 use bevy::app::{PluginGroup, PluginGroupBuilder};
+use crate::core::process::MorldProcessPlugin;
 
 pub mod types;
 pub mod locales;
@@ -12,6 +13,7 @@ impl PluginGroup for MorldCore {
 	fn build(self) -> PluginGroupBuilder {
 		PluginGroupBuilder::start::<Self>()
 			.add(ResInitial)
+			.add(MorldProcessPlugin)
 			.add(FluentInitial)
 	}
 }
