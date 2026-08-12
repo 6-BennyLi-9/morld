6f7027c3c7721ef24ca4fd4beddb04cff6310258
BennyLi <bennyli2011@163.com>
2026-08-12 22:21:15 +0800

    impl for localization(bad solution)

--- src/core/locales.rs
+++ src/core/locales.rs
@@ -1,11 +1,12 @@
 use bevy::app::{App, Plugin, Startup};
-use bevy::asset::LoadedFolder;
-use bevy::prelude::{AssetServer, Commands, Handle, Res, Resource};
-use bevy_fluent::{FluentPlugin, Locale};
+use bevy::asset::{LoadState, LoadedFolder};
+use bevy::prelude::{AssetServer, Commands, Handle, Res, Resource, Update};
+use bevy_fluent::{FluentPlugin, Locale, LocalizationBuilder};
 use unic_langid::langid;
 
 pub struct FluentInitial;
 
+/// The folder of locales. Const.
 #[derive(Resource)]
 pub struct LocaleFolder(Handle<LoadedFolder>);
 
@@ -17,12 +18,26 @@ pub fn load_locales(
 	commands.insert_resource(LocaleFolder(handle));
 }
 
+pub fn update_localizations(
+	mut commands: Commands,
+	localization_builder: LocalizationBuilder,
+	asset_server: Res<AssetServer>,
+	locale_folder: Res<LocaleFolder>,
+) {
+	if let Some(LoadState::Loaded) = asset_server.get_load_state(&locale_folder.0) {
+		let localization = localization_builder.build(&locale_folder.0);
+		commands.remove_resource::<LocaleFolder>();
+		commands.insert_resource(localization);
+	}
+}
+
 impl Plugin for FluentInitial {
 	fn build(&self, app: &mut App) {
         app
 			.add_plugins(FluentPlugin)
 			.insert_resource(Locale::new(langid!("en-US")))
 			.insert_resource(Locale::new(langid!("zh-CN")).with_default(langid!("en-US")))
-			.add_systems(Startup, load_locales);
+			.add_systems(Startup, load_locales)
+			.add_systems(Update, update_localizations);
 	}
 }
